d97f3e6b702028299e0ffab51c00fda897ca23bb
AngeloGSeS <agss2@aluno.ifnmg.edu.br>
2025-09-19 00:29:11 +0000

    Pronto

--- server.js
+++ server.js
@@ -1,29 +1,43 @@
 // ######
 // Local onde os pacotes de dependências serão importados
 // ######
+import pkg from "pg";
+import dotenv from "dotenv";
 import express from "express";      // Requisição do pacote do express
+
 
 // ######
 // Local onde as configurações do servidor serão feitas
 // ######
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
-
+dotenv.config();         // Carrega e processa o arquivo .env
+const { Pool } = pkg;    // Utiliza a Classe Pool do Postgres
 // ######
 // Local onde as rotas (endpoints) serão definidas
 // ######
-app.get("/", (req, res) => {        
-  // Rota raiz do servidor
-  // Rota GET /
-  // Esta rota é chamada quando o usuário acessa a raiz do servidor
-  // Ela retorna uma mensagem com informações do projeto
+app.get("/", async (req, res) => {    
+
 
   console.log("Rota GET / solicitada"); // Log no terminal para indicar que a rota foi acessada
-  
+  // Cria uma nova instância do Pool de conexões com o banco de dados.
+  const db = new Pool({
+    connectionString: process.env.URL_BD,
+  });
+
+  let dbStatus = "ok";
+
+  // Testa a conexão com o banco
+  try {
+    await db.query("SELECT 1");
+  } catch (e) {
+    dbStatus = e.message;
+  }
   // Responde com um JSON contendo uma mensagem
   res.json({
 		descricao: "API para Completar tarefa",    // Substitua pelo conteúdo da sua API
     autor: "Ângelo Gabriel Souza e Silva",     // Substitua pelo seu nome
+    statusBD: dbStatus
   });
 });
 
